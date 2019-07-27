37dff30fae745a9caf3d80b878d365d1e04ddd0d
hamid1856 <50046477+hamid1856@users.noreply.github.com>
2019-07-27 18:46:00 +0430

    create gui for app

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ try:
     import sys
     import selenium_tool
     from colorama import *
+    from tkinter import *
+    from tkinter import ttk
+    from tkinter.ttk import Progressbar
+    from configparser import ConfigParser
+
     init(autoreset=True)
     def isjustspace(string):
         flag_justspace = None
@@ -95,14 +100,12 @@ try:
         else:
             return False
 
-
     def hasscheme(url):
         splittedurl = url.split("/")
         if(splittedurl[0] == "http:" or splittedurl[0] == "https:" and splittedurl[1] == ""):
             return True
         else:
             return False
-
 
     def main():
         #print(Style.BRIGHT + "Trying to install the required libraries...")
@@ -252,7 +255,152 @@ try:
         except:
             print(Style.BRIGHT + Fore.RED + "An unknown error occurred.")
         input()
+
+    def popupmsg(msg):
+        try:
+            t_check.join()
+        except:
+            pass
+        global popup
+        popup = Tk()
+        popup.wm_title("اخطار")
+        NORM_FONT = ("IRANSans 8")
+        global po_label
+        po_label = Label(popup, text=msg, font=NORM_FONT,fg="gray25",bg="white")
+        po_label.pack(side=TOP, fill="x",padx=10,pady=10)
+        global B1
+        B1 = Button(popup,bd=1, font=NORM_FONT, text="باشه",fg="white",bg="gray25", command = popup.destroy)
+        B1.pack(side=TOP,padx=10,pady=10)
+        popup.geometry("260x100")
+        popup.config(bg="white")
+        try:
+            popup.wm_iconbitmap(r'./icon/down_arrow_J6h_icon.ico')
+        except:
+            pass
+        popup.resizable(width=False, height=False)
+        popup.mainloop()
+
+    def downlod_playlist():
+        pass
+
+    def save_playlist():
+        config['optins'] = {
+            'name_entry': list_name,
+            'name_url': list_name,
+            'url': link_name,
+            'number': list_number
+        }
+        with open(r"./save_palylist.ini","w") as s:
+            config.write(s)
+
+    def show_info():
+        popupmsg('''this program has been written by:\nHamidreza Zangoie Known as hamid_1856\nEmail: hrzangoie@gmail.com\nTelegram Chanel: @Sour_software\nversion: 0.0.1 alfa''')   
+
+    def exit_event():
+        
+        try:
+            popup.destroy()
+        except:
+            pass
+        window.destroy()
+
+    def load_playlist():
+        pass
+
+    def language():
+        pass
+
+    Bd = 1
+    bg_color = "medium spring green"
+    fg_color = "snow2"
+    font_ = "IRANSans 9"
+
+    window = Tk()
+    window.title("Aparat Playlint Downloader | by Aidin & Hamids")
+    
+
+    config = ConfigParser()
+    parser = ConfigParser()
+
+    nb = ttk.Notebook(window)
+
+    page1 = Frame(nb)
+    page2 = Frame(nb)
+
+    nb.add(page1, text=' صفحه اصلی ')
+    nb.add(page2, text=' تنظیمات ')
+
+
+
+
+    frame1 = LabelFrame(page1,bd=0)
+    frame1.pack(pady=(5,5),padx=(5,5),fill=BOTH)
+    frame2 = LabelFrame(page1,bd=0)
+    frame2.pack(pady=(5,0),padx=(5,5),expand=1,fill=BOTH)
+    frame4 = LabelFrame(frame2,bd=0)
+    frame4.pack(side=TOP,anchor=W,expand=1,fill=BOTH)
+    frame3 = LabelFrame(frame2,bd=0)
+    frame3.pack(side=TOP,anchor=W,fill=BOTH)
+
+    progress = Progressbar(frame3, orient=HORIZONTAL,length=100,  mode='determinate')
+    progress.pack(side=TOP,anchor=W,padx=(5,5),pady=(5,5),fill=BOTH,expand=1)
+
+    p_p = Label(progress,text="%0",bg='gray90')
+    p_p.pack(anchor='center',padx=(0,5),pady=(5,5))
+
+    listbox = Listbox(frame4,width=87,bd=Bd,height=12,fg=fg_color,bg=bg_color,font=font_)
+    listbox.pack(side=TOP,anchor=N,padx=(5,5),pady=5,fill=BOTH)
+
+    Label(frame1,text="آدرس:",font=font_).pack(side=LEFT,anchor=W,padx=(5,0),pady=(5,5))
+
+    url_entry = Entry(frame1,font=font_,bd=Bd)
+    url_entry.pack(side=LEFT,anchor=N,padx=(5,0),pady=(5,5),expand=1,fill=BOTH)
+
+    btn_search = Button(frame1,text="جوست و جو",fg=fg_color,bg=bg_color,font=font_,bd=Bd)
+    btn_search.pack(side=LEFT,anchor=E,padx=(5.5),pady=(5,5))
+
+    Label(page2,text="کیفیت",font=font_).pack(side=TOP,anchor=W)
+
+    cb = ttk.Combobox(page2,state='readonly',font=font_,values=("پایین ترین","بالا ترین"))
+    cb.set("پایین ترین")
+    cb.pack(side=TOP,anchor=W,padx=5,pady=(0,5))
+
+    D_button = Button(frame3,text="دانلود پلی لیست",state='disabled',fg=fg_color,bg=bg_color,font=font_,bd=Bd)
+    D_button.pack(side=RIGHT,anchor=NW,padx=(5,5),pady=(5,5))
+
+    S_button = Button(frame3,text="ذخیره پلی لیست",state='disabled',fg=fg_color,bg=bg_color,font=font_,bd=Bd)
+    S_button.pack(side=RIGHT,anchor=NW,padx=(5,5),pady=(5,5))
+
+    # create a toplevel 
+    # menu
+    menubar = Menu(window)
+
+    # create a pulldown menu, and add it to the menu bar
+    filemenu = Menu(menubar, tearoff=0)
+    filemenu.add_command(label="بارگذاری پلی لیست", command=load_playlist)
+    filemenu.add_command(label="ذخیره پلی لیست", command=save_playlist)
+    filemenu.add_separator()
+    filemenu.add_command(label="خروج", command=exit_event)
+    menubar.add_cascade(label="فایل", menu=filemenu)
+
+
+    helpmenu = Menu(menubar, tearoff=0)
+    helpmenu.add_command(label="اطلاعات")
+    helpmenu.add_separator()
+    helpmenu.add_command(label="درباره")
+    menubar.add_cascade(label="کمک", menu=helpmenu)
+
+    window .config(menu=menubar)
+
+    nb.pack(expand=1, fill="both",padx=5,pady=5)
+
+
 except:
     print(Style.BRIGHT + Fore.RED + "An unknown error occurred.")
+
 if __name__ == "__main__":
-    main()
+    window.protocol("WM_DELETE_WINDOW", exit_event)
+    window.iconbitmap(r"./icon/down_arrow_J6h_icon.ico")
+    window.resizable(width=False,height=False)
+    #window.geometry("700x500")
+    window.mainloop()
